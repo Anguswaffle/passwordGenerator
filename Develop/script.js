027342156baefd909ad1441bb81b317bc211aa88
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-10-12 18:24:18 -0700

    Finished code

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,3 @@
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -37,50 +21,53 @@ function getRandomInt(max) {
 
 function generatePassword() {
 
+  // Empty local password
   var localPassword = "";
 
-  var passLength = prompt("Choose a number between 8 and 128 for your password length");
-
-  // Error catching incorrect inputs
-  if(!passLength) {
-    return;
-  } else if(passLength < 8 || passLength > 128) {
-    alert("Please enter a number between 8 and 128");
-    return;
-  }
-
-  var hasLowerCase = confirm("Would you like your password to have lowercase characters?");
-  var hasUpperCase = confirm("Would you like your password to have uppercase characters?");
-  var hasNumbers = confirm("Would you like your password to have numbers?");
-  var hasSpecialChars = confirm("Would you like your password to have special characters?");
-
+  // Possible character bank values
   var lowAlphabet = "abcdefghijklmnopqrstuvwxyz";
   var upAlphabet = lowAlphabet.toUpperCase();
   var numbers = "1234567890";
   var specialChars = "!@#$%^&*()_+=?/";
 
+  // Character bank begins as empty string
   var charBank = "";
 
-  if(hasLowerCase) {
-    charBank = charBank.concat(lowAlphabet);
-  }
-  
-  if(hasUpperCase) {
-    charBank = charBank.concat(upAlphabet);
+  var passLength = prompt("Choose a number between 8 and 128 for your password length");
+
+  // Checks to see if desired password length is within parameters
+  if (passLength >= 8 && passLength <= 128) {
+
+    // Adds characters to character bank depending on user input
+    if (confirm("Would you like your password to have lowercase characters?")) {
+      charBank = charBank.concat(lowAlphabet);
+    }
+
+    if (confirm("Would you like your password to have uppercase characters?")) {
+      charBank = charBank.concat(upAlphabet);
+    }
+
+    if (confirm("Would you like your password to have numbers?")) {
+      charBank = charBank.concat(numbers);
+    }
+
+    if (confirm("Would you like your password to have special characters?")) {
+      charBank = charBank.concat(specialChars);
+    }
+
+    for (i = 0; i < passLength; i++) {
+      localPassword = localPassword.concat(charBank.charAt(getRandomInt(charBank.length - 1)));
+    }
+
+    return localPassword;
   }
 
-  if(hasNumbers) {
-    charBank = charBank.concat(numbers);
-  }
-
-  if(hasSpecialChars) {
-    charBank = charBank.concat(specialChars);
-  }
-
-  for(i = 0 ; i < passLength ; i++){
-    localPassword = localPassword.concat(charBank.charAt(getRandomInt(charBank.length - 1)));
-  }
-
-  return localPassword;
+  // Error catching incorrect inputs
+  else if (!passLength) {
+    return;
+  } else {
+    alert("Please enter a number between 8 and 128");
+    return;
+  } 
 
 }
